d90dfc1620560717cecca289bb3a4551f693f822
Andrey "Limych" Khrolenok <andrey@khrolenok.ru>
2017-10-06 07:13:49 +0000

    Clean develop assets

--- after.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# If you would like to do some extra provisioning you may
-# add any commands you wish to this file and they will
-# be run after the Homestead machine is provisioned.
-
-git config --global core.excludesfile ~/.gitignore
-echo .idea/ >~/.gitignore
